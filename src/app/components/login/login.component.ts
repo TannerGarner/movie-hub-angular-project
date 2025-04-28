586956a0f0977604d530a7e01c37564946c644f8
tannergarner <garnert.w.14@gmail.com>
2025-04-28 10:13:44 -0600

    Add login styles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,8 +14,9 @@ export class LoginComponent {
     password: ''
   }
   authService: AuthService;
-
-  constructor(private auth: AuthService) {
+  router: Router;
+  constructor(router: Router, auth: AuthService) {
+    this.router = router;
     this.authService = auth;
   }
 
@@ -27,6 +29,7 @@ export class LoginComponent {
       this.authService.login(this.userLogin.username, this.userLogin.password)
         .then(() => {
           alert('Login successful!');
+          this.router.navigate(['/home']);
         })
         .catch((error) => {
           alert(`Login Failed: ${error}`);
