4df862d4d6f7f4b950e333fe50ac1e44f7a79dc8
Texas R3X <texasr3xphone@gmail.com>
2025-05-08 09:52:51 -0600

    Added MovieCardsComponent.goToMovieDetails() | Movie cards have click functionality

--- src/app/components/movie-details-page/movie-details-page.component.ts
+++ src/app/components/movie-details-page/movie-details-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { TmdbService } from '../../services/tmdb.service';
 import { JsonPipe, DatePipe } from '@angular/common';
 import { TmdbVideo, TmdbVideoResponse } from '../../interfaces/tmdb-video.interface';
@@ -18,15 +18,11 @@ export class MovieDetailsPageComponent implements OnInit {
   comments: string[] = [];
   movieId: number = 0;
   collection: any = null;
-  router: Router;
 
   constructor(
     private route: ActivatedRoute,
-    private tmdbService: TmdbService,
-    router: Router
-  ) {
-    this.router = router;
-  }
+    private tmdbService: TmdbService
+  ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -67,10 +63,5 @@ export class MovieDetailsPageComponent implements OnInit {
       this.comments.push(this.comment);
       this.comment = '';
     }
-  }
-
-  goToMovieDetails(movieId: number) {
-    this.router.navigate(['/movie', movieId]);
-    window.scrollTo(0, 0);    
   }
 }
