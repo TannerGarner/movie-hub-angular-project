cbb514cf04b39c92a4159ae9efa7ef8bbb5719f5
Texas R3X <texasr3xphone@gmail.com>
2025-05-11 16:39:52 -0600

    Cleaned up MovieDetailsPageComponent

--- src/app/components/movie-details-page/movie-details-page.component.ts
+++ src/app/components/movie-details-page/movie-details-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { TmdbService } from '../../services/tmdb.service';
 import { FirestoreService } from '../../services/firestore.service';
 import { TmdbVideo, TmdbVideoResponse } from '../../interfaces/tmdb-video.interface';
+import { Comment } from '../../interfaces/comment.interface'
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -16,14 +17,11 @@ export class MovieDetailsPageComponent implements OnInit, OnDestroy {
   public movieDetails: any = null;
   youtubeURL: string = '';
   comment: string = '';
-  // comments: any[] = [];
   movieId: number = 0;
   collection: any = null;
-  router: Router;
   watchData$: Observable<any[]>;
   private watchDataSubscription: Subscription | undefined;
-  comments$: Observable<any[]> | undefined;
-  // private commentsSubscription: Subscription | undefined;
+  comments$: Observable<Comment[]> | undefined;
   hasWatched: boolean = false;
   onWatchList: boolean = false;
   rating: number = 0;
@@ -32,11 +30,8 @@ export class MovieDetailsPageComponent implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private tmdbService: TmdbService,
-    private firestoreService: FirestoreService,
-    // private cdr: ChangeDetectorRef,
-    router: Router
+    private firestoreService: FirestoreService
   ) {
-    this.router = router;
     this.watchData$ = this.firestoreService.getWatchData();
   }
 
@@ -70,26 +65,9 @@ export class MovieDetailsPageComponent implements OnInit, OnDestroy {
     });
   }
 
-  // private setupCommentsSubscription(): void {
-  //   console.log("0 this.comments:", this.comments);
-  //   this.commentsSubscription = this.firestoreService
-  //     .getAllComments(this.movieId)
-  //     .subscribe(comments => {
-  //       this.comments = comments;
-  //       this.cdr.detectChanges();
-  //       console.log("this.comments:", this.comments);
-  //     });
-
-  //   // this.comments$ = this.firestoreService.getAllComments(this.movieId);
-  //   // this.commentsSubscription = this.comments$.subscribe(comments => {
-  //   //   this.comments = comments;
-  //   //   console.log("this.comments:", this.comments);
-  //   // });
-  // }
-
   private loadMovieDetails() {
     this.tmdbService.getMovieById(this.movieId).subscribe((data) => {
-      console.log(data);
+      console.log('data:', data);
       this.movieDetails = data;
       
       if (this.movieDetails.belongs_to_collection) {
@@ -181,19 +159,13 @@ export class MovieDetailsPageComponent implements OnInit, OnDestroy {
     }
   }
 
-  deleteComment(commentId: string) {
-    // this.firestoreService.deleteComment(commentId)
-    //   .then(() => {
-    //     console.log('Comment deleted successfully');
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error deleting comment:', error);
-    //   });
-  }
-
-
-  goToMovieDetails(movieId: number) {
-    this.router.navigate(['/movie', movieId]);
-    window.scrollTo(0, 0);    
-  }
+  // deleteComment(commentId: string) {
+  //   this.firestoreService.deleteComment(commentId)
+  //     .then(() => {
+  //       console.log('Comment deleted successfully');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error deleting comment:', error);
+  //     });
+  // }
 }
